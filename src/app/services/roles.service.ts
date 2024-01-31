f8c3f4874209c51d8b19e79e9f003ca786e13fd9
RubenE107 <gomr030918@gs.utm.mx>
2024-01-31 03:43:17 -0600

    se

---
+++ src/app/services/roles.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Importa HttpClient de @angular/common/http.
+import { environment } from '../environments/environment'; // Importa environment de la ruta especificada.
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolesService {
+
+constructor(private http:HttpClient) {}
+  list() {
+    return this.http.get(`${environment.API_URL}usuarios/mostrarTodosUsuarios`);
+  }
+}

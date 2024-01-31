c452fbf99bd990777826ec3733fb23cb0ab6829d
RubenE107 <gomr030918@gs.utm.mx>
2024-01-31 01:58:28 -0600

    se

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -88,9 +88,10 @@ export class LoginComponent implements  OnInit{
   }else{
     console.log("usuario: ", this.usuario);
     this.usuarioService.create(this.usuario.Nombre,this.usuario.Apellido,this.usuario.correo,this.usuario.Contrasena,this.usuario.FechaNacimiento,this.usuario.Telefono).subscribe((resUsuario: any) => {
-      this.router.navigateByUrl("/login");
+     
     },err => console.error(err)
     );
+    this.router.navigate(['login']);
   }
   
 }

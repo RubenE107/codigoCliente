cc6d6a26305dcdb0cf3c278d175db9ccbe56946e
RubenE107 <gomr030918@gs.utm.mx>
2024-02-12 05:33:57 -0600

    c

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -125,9 +125,9 @@ export class CarritoComponent implements OnInit{
   }
   agr(){
     let carrID=localStorage.getItem('CarritoID');
-    
+    console.log("editar id: ", carrID);
     console.log(this.carrito);
-    this.carritoService.agregarProducto(carrID,this.carrito.Cantidad,this.carrito.MembresiaID,this.carrito.PrecioUnitario,this.carrito.Total).subscribe((resusuario: any) =>
+    this.carritoService.agregarProducto(carrID,this.carrito.Cantidad,this.carrito.MembresiaID,this.carrito.PrecioUnitario,this.carrito.).subscribe((resusuario: any) =>
     {
       console.log(resusuario);
       this.router.navigate(['/principal/shopping']);

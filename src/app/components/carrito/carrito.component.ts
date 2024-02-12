308b54a643cf701844acf610e50e6a7719bd2e91
RubenE107 <gomr030918@gs.utm.mx>
2024-02-12 02:19:14 -0600

    c

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { CarritoService } from '../../services/carrito.service';
 import { Carrito } from '../../models/Carrito';
 import { Router } from '@angular/router';
-
+declare var $: any;
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
-export class CarritoComponent {
+export class CarritoComponent implements OnInit{
   carritos : Carrito [] = [];
   carrito :Carrito= new Carrito();
     constructor(private datePipe:DatePipe  , private carritoService: CarritoService, private router: Router){
@@ -24,9 +24,23 @@ export class CarritoComponent {
       err => console.error(err)
       );
       
-    
+      
       
     }
+    ngOnInit(): void 
+      {
+        $('.dropdown-trigger').dropdown();
+        $('.tooltipped').tooltip();
+        $(document).ready(function()
+        {
+          $('.modal').modal();
+    
+        });
+    
+        
+    
+    
+      }
     FormatDate(date: Date | null): string {
       if (date) {
           return this.datePipe.transform(date, 'yyyy-MM-dd') || '';
